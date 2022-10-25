917bff06ecde273792b178673c9608216fba6725
Denis2752935 <Denis2752935@ya.ru>
2022-10-25 11:58:22 +0500

    2 commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager vassal = new SalesManager(int[] sales1);
+
+        vassal.max(4);
+
+
+        //commit
+    }
+}
